be1131fc31cb9f848445960b8fa8f0564db01634
Kunal Biraris <birariskunalqa@gmail.com>
2024-01-17 23:32:09 +0100

    added RemoveDuplicateWordsFromSentence class

---
+++ src/RemoveDuplicateWordsFromSentence.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class RemoveDuplicateWordsFromSentence {
+    public static void main(String[] args) {
+
+        String originalString = "Remove duplicate word word Remove";
+
+        System.out.println(removeDuplicateFromString(originalString));
+    }
+
+    static String removeDuplicateFromString(String originalString) {
+
+        String[] splitString = originalString.split(" ");
+
+        HashSet<String> setString = new HashSet<>();
+
+        StringBuilder resultBuffer  = new StringBuilder();
+
+        for(String word : splitString){
+            if(setString.add(word)){
+                resultBuffer.append(word).append(" ");
+            }
+        }
+        return resultBuffer.toString();
+    }
+}
