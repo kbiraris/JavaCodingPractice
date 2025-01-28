5706b6bfaa8cf45e8f24ecec9204cb522d85e5de
Kunal Biraris <birariskunalqa@gmail.com>
2025-01-28 12:16:46 +0100

    added solution with for loop as well along with recursion

--- src/SumOfArrayRecursion.java
+++ src/SumOfArrayRecursion.java
@@ -1,16 +1,21 @@
 public class SumOfArrayRecursion {
 
    static int[] arr = {10,2,3,50,4,1};
-   static int methodExecutionCount = 1;
 
     public static void main(String[] args) {
-        //System.out.println(arr.length);
-
+        //sum of the array using recursion
         int sumOfArrayVariable = sumOfArray(arr.length-1);
-        System.out.println("Sum of array is " + sumOfArrayVariable);
+        System.out.println("Using recursion Sum of array is " + sumOfArrayVariable);
+
+        //sum of the array using for loop
+        int sum=0;
+        for(int i=0; i<arr.length;i++){
+            sum = sum + arr[i];
+        }
+        System.out.println("Using for loop sum of array is " + sum);
     }
+
     private static int sumOfArray(int indexOfArray){
-        System.out.println("sumOfArray method execution " + methodExecutionCount++);
         if(indexOfArray==0)
         {
             return arr[indexOfArray];
